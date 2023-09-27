5212c8fefafd0c0392bb85079fd13e854656354d
Minasuki <jossiel2023@gmail.com>
2023-09-27 01:16:00 -0700

    Creacion del Drawer

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,16 @@
-import { Button, ThemeProvider, Typography } from "@mui/material";
+import { ThemeProvider } from "@mui/material";
 import {} from "react";
-import Theme from './components/Theme'
-
+import Theme from "./components/Theme";
+import Navbar from './components/Navbar1'
+import Listas from './components/Listas'
 
 function App() {
-  
-
   return (
     <ThemeProvider theme={Theme}>
-      <div>
-        <Typography variant="h1" color="initial">
-          hola mundo
-        </Typography>
-        <Typography variant="body1" gutterBottom align="center">
-          body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
-          blanditiis tenetur unde suscipit, quam beatae rerum inventore
-          consectetur, neque doloribus, cupiditate numquam dignissimos laborum
-          fugiat deleniti? Eum quasi quidem quibusdam.
-        </Typography>
-      </div>
-      <Button variant="contained" color="primary">
-        Boton 1
-      </Button>
-      <Button variant="contained" color="secondary">
-        Boton 2
-      </Button>
-      <Button variant="otro" color="info" size="large">
-        Algo
-      </Button>
-      <Button variant="diseñado" color="inherit" size="large">
-        Boton con estilo
-      </Button>
+      <Navbar/>
+      <Listas/>
     </ThemeProvider>
+   
   );
 }
 
---
+++ src/components/Main.jsx
@@ -0,0 +1,32 @@
+import { Box, Button, Typography } from "@mui/material";
+
+const Main = () => {
+    return ( 
+        <Box>
+        <Typography variant="h1" color="initial">
+          hola mundo
+        </Typography>
+        <Typography variant="body1" gutterBottom align="center">
+          body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+          blanditiis tenetur unde suscipit, quam beatae rerum inventore
+          consectetur, neque doloribus, cupiditate numquam dignissimos laborum
+          fugiat deleniti? Eum quasi quidem quibusdam.
+        </Typography>
+     
+      <Button variant="contained" color="primary">
+        Boton 1
+      </Button>
+      <Button variant="contained" color="secondary">
+        Boton 2
+      </Button>
+      <Button variant="otro" color="info" size="large">
+        Algo
+      </Button>
+      <Button variant="diseñado" color="inherit" size="large">
+        Boton con estilo
+      </Button>
+      </Box>
+     );
+}
+ 
+export default Main;
